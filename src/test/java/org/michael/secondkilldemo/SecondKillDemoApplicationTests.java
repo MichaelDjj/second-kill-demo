166fa31c49d8e4e261f2a44267579c6c3cecb9df
MichaelDjj <michael.djj@gmail.com>
2022-06-06 17:25:31 +0800

    Initial commit

---
+++ src/test/java/org/michael/secondkilldemo/SecondKillDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.michael.secondkilldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondKillDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
